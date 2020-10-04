bcb10ba890ab889ca4b941455ebf7cf6e0da7117
Pallav Agarwal <agpallav@gmail.com>
2020-10-05 01:33:15 +0800

    Removed Template Files

--- frontend/app/src/main/java/com/purdue/comedia/ui/main/PageViewModel.kt
+++ frontend/app/src/main/java/com/purdue/comedia/ui/main/PageViewModel.kt
@@ -1,19 +1,21 @@
 package com.purdue.comedia.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+/** NOTE FROM PALLAV: This code is only for safekeeping. **/
 
-class PageViewModel : ViewModel() {
-
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
-    }
-
-    fun setIndex(index: Int) {
-        _index.value = index
-    }
-}
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.Transformations
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.ViewModelProvider
+//
+//class PageViewModel : ViewModel() {
+//
+//    private val _index = MutableLiveData<Int>()
+//    val text: LiveData<String> = Transformations.map(_index) {
+//        "Hello world from section: $it"
+//    }
+//
+//    fun setIndex(index: Int) {
+//        _index.value = index
+//    }
+//}

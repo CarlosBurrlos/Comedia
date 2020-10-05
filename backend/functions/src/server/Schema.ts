84ba6837eb8e40bf759972c382cd6bc74dd5b3e7
Arkinion <nicholasgr44@gmail.com>
2020-10-05 00:34:44 -0400

    Removed unnecessary things

--- backend/functions/src/server/Schema.ts
+++ backend/functions/src/server/Schema.ts
@@ -1,166 +1,31 @@
 import "reflect-metadata";
-import {Arg, buildSchemaSync, Field, FieldResolver, Query, Resolver} from "type-graphql";
+import {Arg, buildSchemaSync, Query, Resolver} from "type-graphql";
 import User from "./schema/User";
 import Post from "./schema/Post";
 import Comment from "./schema/Comment";
 import Profile from "./schema/Profile";
-import PostType from "./schema/PostType";
-
-// @Resolver()
-// class ComediaResolver {
-//     @Query((returns?: void) => [User])
-//     async users(): Promise<User[]> {
-//         return [];
-//     }
-//
-//     @Query((returns?: void) => [Post])
-//     async posts(): Promise<Post[]> {
-//         return [];
-//     }
-//
-//     @Query((returns?: void) => [Comment])
-//     async comments(): Promise<Comment[]> {
-//         return [];
-//     }
-//
-//     @Query((returns?: void) => [Profile])
-//     async profiles(): Promise<Profile[]> {
-//         return [];
-//     }
-// }
 
 @Resolver((of?: void) => User)
 class UserResolver {
-    @FieldResolver()
-    username(): string {
-        return '';
-    }
-
-    @FieldResolver()
-    email(): string {
-        return '';
-    }
-
-    @FieldResolver()
-    password(): string {
-        return '';
-    }
-
-    @FieldResolver((type?: void) => [User])
-    usersFollowing(): User[] {
-        return [];
-    }
-
-    @FieldResolver((type?: void) => [User])
-    followers(): User[] {
-        return [];
-    }
-
-    @FieldResolver()
-    genresFollowing(): string[] {
-        return [];
-    }
-
-    @FieldResolver((type?: void) => [Post])
-    createdPosts(): Post[] {
-        return [];
-    }
-
-    @Field((type?: void) => [Post])
-    savedPosts(): Post[] {
-        return [];
-    }
-
-    @FieldResolver((type?: void) => [Comment])
-    comments(): Comment[] {
-        return [];
-    }
-
-    @FieldResolver()
-    profile(): Profile {
-        return new Profile();
-    }
 }
 
 @Resolver((of?: void) => Post)
 class PostResolver {
-    @FieldResolver()
-    poster(): User {
-        return new User();
-    }
-
-    @FieldResolver()
-    genre(): string {
-        return '';
-    }
-
-    @FieldResolver()
-    type(): PostType {
-        return PostType.TEXT;
-    }
-
-    @FieldResolver()
-    content(): string {
-        return '';
-    }
-
-    @FieldResolver((type?: void) => [Comment])
-    comments(): Comment[] {
-        return [];
-    }
-
-    @FieldResolver()
-    upvoteCount(): number {
-        return -1;
-    }
-
-    @FieldResolver()
-    downvoteCount(): number {
-        return -1;
-    }
 }
 
 @Resolver((of?: void) => Comment)
 class CommentResolver {
-    @FieldResolver()
-    poster(): User {
-        return new User();
-    }
-
-    @FieldResolver()
-    parent(): Post {
-        return new Post();
-    }
-
-    @FieldResolver()
-    content(): string {
-        return '';
-    }
 }
 
 @Resolver((of?: void) => Profile)
 class ProfileResolver {
-    @FieldResolver()
-    user(): User {
-        return new User();
-    }
-
-    @FieldResolver()
-    profileImage(): string {
-        return '';
-    }
-
-    @FieldResolver()
-    biography(): string {
-        return '';
-    }
 }
 
 @Resolver()
 class BasicResolver {
     @Query(() => [User])
     users(@Arg('ids', () => [String]) ids: string[]): User[] {
-        return [];
+        return [ new User() ];
     }
 
     @Query(() => [Post])

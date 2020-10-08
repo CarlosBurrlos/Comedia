b7846ec0c699f0a15d43ed19097e27a738d567e5
Arkinion <nicholasgr44@gmail.com>
2020-10-07 23:04:48 -0400

    Added emulation for firestore

--- frontend/app/src/test/java/com/purdue/comedia/ExampleUnitTest.kt
+++ frontend/app/src/test/java/com/purdue/comedia/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.purdue.comedia
 
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +14,34 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    lateinit var firestore: FirebaseFirestore
+
+    @Before
+    fun setupEmulators() {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.useEmulator("localhost", 8080)
+
+        addTestUser(firestore)
+    }
+
+    private fun addTestUser(firestoreInstance: FirebaseFirestore) {
+        val user = firestoreInstance.collection("users").document("test")
+        val profile = firestoreInstance.collection("profiles").document("test")
+
+        val userModel = UserModel()
+        userModel.username = "test"
+        userModel.email = "test@test.com"
+        userModel.profile = profile
+
+        val profileModel = ProfileModel()
+        profileModel.biography = "testing"
+        profileModel.profileImage = "test.com/test.jpg"
+        profileModel.user = user
+
+        user.set(userModel)
+        profile.set(profileModel)
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)

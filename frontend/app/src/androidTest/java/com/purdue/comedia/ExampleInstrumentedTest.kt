7d797f80d08d783b8694dd5df50e6f26dc1709ec
Arkinion <nicholasgr44@gmail.com>
2020-11-03 20:35:09 -0500

    Added commented out unit tests and changed code for signin/signout button

--- frontend/app/src/androidTest/java/com/purdue/comedia/ExampleInstrumentedTest.kt
+++ frontend/app/src/androidTest/java/com/purdue/comedia/ExampleInstrumentedTest.kt
@@ -1,13 +1,18 @@
 package com.purdue.comedia
 
+import android.view.View
+import androidx.fragment.app.FragmentTransaction
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.firebase.auth.FirebaseAuth
+import org.hamcrest.Matcher
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -113,4 +118,64 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.radioBtnUrl)).perform(click())
         onView(withId(R.id.postBodyField)).check(matches(withHint("Enter a joke website URL")))
     }
+    /*
+    @Test
+    fun testFollowGenre() {
+        signIn()
+
+        ActivityScenario.launch(MainActivity::class.java)
+        FragmentTransaction.TRANSIT_ENTER_MASK
+        onView(withId(R.id.btnBrowseGenre)).perform(click())
+        Thread.sleep(1000)
+        onView(withHint("Genre: Eg. Pun")).perform(typeText("TestPun"))
+        onView(withText("CONFIRM")).perform(click())
+        Thread.sleep(5000)
+
+        signOut()
+    }
+
+    private fun signIn() {
+        ActivityScenario.launch(SignUp::class.java)
+        onView(withId(R.id.btnToggleRegister)).perform(click())
+        onView(withId(R.id.registerUsername)).perform(typeText("pallav"))
+        onView(withId(R.id.registerPassword)).perform(typeText("123456"))
+        onView(withId(R.id.btnRegister)).perform(click())
+
+        Thread.sleep(3000)
+    }
+
+    private fun signOut() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.btnToLoginPage)).check(matches(withText("Sign Out")))
+        FirebaseAuth.getInstance().signOut()
+        Thread.sleep(2000)
+    }
+
+    fun clickItemWithId(id: Int): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View>? {
+                return null
+            }
+
+            override fun getDescription(): String {
+                return "Click on a child view with specified id."
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val v = view.findViewById(id) as View
+                v.performClick()
+            }
+        }
+    }
+
+    @Test
+    fun testViewUserProfile() {
+        signIn()
+
+        ActivityScenario.launch(MainActivity::class.java)
+        Thread.sleep(5000)
+
+        signOut()
+    }
+    */
 }

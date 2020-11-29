665ae8fbea44a9443efdeebb59960d60fa2e5c0a
Tom Yarrow <tyarrow1@gmail.com>
2020-11-29 14:22:22 -0600

    Find/sort relevancy of users

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,4 +1,4 @@
-import admin from 'firebase-admin';
+import admin, { firestore } from 'firebase-admin';
 import {https} from "firebase-functions";
 import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 // import createServer from './server/CreateGraphQLServer';
@@ -10,7 +10,7 @@ import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 // const admin = require('firebase-admin');
 admin.initializeApp();
 
-const firestore = admin.firestore();
+const firestoreInstance = admin.firestore();
 
 //const server = createServer();
 
@@ -31,43 +31,66 @@ const firestore = admin.firestore();
 //     res.json({result: `Message with ID: ${writeResult.id} added.`});
 // });
 
-function commonLength(array1: any[], array2: any[]): number {
-    return array1.filter(value => array2.includes(value)).length;
-}
-
 const resolvers: {[index: string]: (req: https.Request) => Promise<object>} = {
     '/relevantUsers': relevantUsers,
     '/relevantPosts': relevantPosts,
 };
 
-// This was me late at night trying to write a function, it's probably all wrong
 exports.calcRelevancyUser = https.onRequest(async (req, res) => {
     const response = await resolvers[req.path](req);
     res.json(response);
 });
 
-async function relevantUsers(req: https.Request): Promise<object> {
-    // TODO: Return users sorted by relevance
-    return {};
+interface UserRelevancyPair {
+    doc_snap: firestore.DocumentSnapshot,
+    doc_rel: number
 }
 
-// NOTE: The params here may change depending on what we need it to do
-async function calculateUserRelevancy(req: https.Request): Promise<number> {
-    // Grab the parameters
-    if (!(req.query.user && req.query.target)) return -1;
-
+async function relevantUsers(req: https.Request): Promise<object> {
+    // Get query parameters
+    if (!(req.query.user && req.query.mode)) return {};
     const uid: string = req.query.user?.toString();
-    const t_uid: string = req.query.target?.toString();
+    const mode: string = req.query.mode?.toString();
 
-    // Get the documents
-    const user = await firestore.collection('users').doc(uid).get();
-    const target = await firestore.collection('users').doc(t_uid).get();
+    // Get the user and the set of target users to use
+    const user = await firestoreInstance.collection('users').doc(uid).get();
+    let targets: firestore.DocumentReference[];
+    if (mode === "followers") {
+        targets = user.get("followers");
+    } else if (mode === "following") {
+        targets = user.get("usersFollowing");
+    } else {
+        return {};
+    }
 
+    // Create the list of objects
+    let UserRelevancyPairs: UserRelevancyPair[] = await Promise.all(targets.map(
+        async (target: firestore.DocumentReference) => {
+            let target_snap = await target.get();
+            let new_pair: UserRelevancyPair = {
+                doc_snap: target_snap,
+                doc_rel: await calculateUserRelevancy(user,target_snap)
+            };
+            return new_pair;
+        }
+    ));
+
+    // Sort by relevancy and return the document snapshots
+    UserRelevancyPairs.sort((a,b) => a.doc_rel - b.doc_rel);
+    return UserRelevancyPairs.map(pair => pair.doc_snap);
+}
+
+function commonLength(array1: any[], array2: any[]): number {
+    return array1.filter(value => array2.includes(value)).length;
+}
+
+async function calculateUserRelevancy(user: firestore.DocumentSnapshot, target: firestore.DocumentSnapshot): Promise<number> {
+    // Default relevancy is zero
     let rel = 0;
     if (user.exists && target.exists) {
         // calculate relevancy
-        let user_data = user.data()
-        let target_data = target.data()
+        let user_data = user.data();
+        let target_data = target.data();
 
         let rel_mult = 1;
         if (user.get("usersFollowing").contains(target.ref)) {
@@ -77,19 +100,23 @@ async function calculateUserRelevancy(req: https.Request): Promise<number> {
             rel_mult *= 1.5;
         }
 
+        let targetPosts: firestore.DocumentReference[] = target.get("createdPosts");
+
         // The number of times the user has upvoted the target's posts
-        rel += commonLength(user.get("upvotedPosts"),target.get("createdPosts"));
+        rel += commonLength(user.get("upvotedPosts"), targetPosts);
         
         // The number of times the user has downvoted the target's posts
-        rel -= commonLength(user.get("downvotedPosts"),target.get("createdPosts"));
+        rel -= commonLength(user.get("downvotedPosts"), targetPosts);
 
         // The number of times the user has commented on the target's posts
-        //rel += commonLength(user.get("upvoteList"),target.get("createdPosts"));
+        let comment_list: firestore.DocumentReference[] = user.get("comments");
+        let post_list = comment_list.map(async x => await x.get().then(y => y.get("parent")));
+        rel += 2 * commonLength(post_list, targetPosts);
 
-        rel *= rel_mult;
-        // TODO: Calculate relevancy for two users
+        if (rel > 0) {
+            rel *= rel_mult;
+        }
     }
-
     return rel;
 }
 

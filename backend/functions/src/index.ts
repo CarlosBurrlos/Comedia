bce853a3fe6b2c4a85f71dec5929a78293afcf16
Arkinion <nicholasgr44@gmail.com>
2020-11-29 18:19:15 -0500

    Added in sorting and fetching of posts by relevance

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,6 +1,8 @@
 import admin from 'firebase-admin';
 import {https} from "firebase-functions";
-import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+import DocumentReference = admin.firestore.DocumentReference;
+import DocumentSnapshot = admin.firestore.DocumentSnapshot;
+import Timestamp = admin.firestore.Timestamp;
 // import createServer from './server/CreateGraphQLServer';
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
@@ -35,7 +37,7 @@ function commonLength(array1: any[], array2: any[]): number {
     return array1.filter(value => array2.includes(value)).length;
 }
 
-const resolvers: {[index: string]: (req: https.Request) => Promise<object>} = {
+const resolvers: { [index: string]: (req: https.Request) => Promise<object> } = {
     '/relevantUsers': relevantUsers,
     '/relevantPosts': relevantPosts,
 };
@@ -56,8 +58,8 @@ async function calculateUserRelevancy(req: https.Request): Promise<number> {
     // Grab the parameters
     if (!(req.query.user && req.query.target)) return -1;
 
-    const uid: string = req.query.user?.toString();
-    const t_uid: string = req.query.target?.toString();
+    const uid: string = req.query.user.toString();
+    const t_uid: string = req.query.target.toString();
 
     // Get the documents
     const user = await firestore.collection('users').doc(uid).get();
@@ -78,10 +80,10 @@ async function calculateUserRelevancy(req: https.Request): Promise<number> {
         }
 
         // The number of times the user has upvoted the target's posts
-        rel += commonLength(user.get("upvotedPosts"),target.get("createdPosts"));
-        
+        rel += commonLength(user.get("upvotedPosts"), target.get("createdPosts"));
+
         // The number of times the user has downvoted the target's posts
-        rel -= commonLength(user.get("downvotedPosts"),target.get("createdPosts"));
+        rel -= commonLength(user.get("downvotedPosts"), target.get("createdPosts"));
 
         // The number of times the user has commented on the target's posts
         //rel += commonLength(user.get("upvoteList"),target.get("createdPosts"));
@@ -93,7 +95,141 @@ async function calculateUserRelevancy(req: https.Request): Promise<number> {
     return rel;
 }
 
-async function relevantPosts(req: https.Request): Promise<object> {
-    // TODO: Return posts sorted by relevance
-    return {};
+interface UserModel {
+    comments: DocumentReference[];
+    createdPosts: DocumentReference[];
+    downvotedPosts: DocumentReference[];
+    email: string;
+    followers: DocumentReference[];
+    genresFollowing: string[];
+    profile: DocumentReference;
+    savedPosts: DocumentReference[];
+    username: string;
+    usersFollowing: DocumentReference[];
+}
+
+interface GenreModel {
+    posts: DocumentReference[];
+}
+
+class RelevantPostResponse {
+    posts: PostModel[] = [];
+}
+
+type VariableDepthArray<T> = T | VariableDepthArray<T>[];
+
+function flatten<T>(arr: VariableDepthArray<T>[], result: T[] = []): T[] {
+    for (let i = 0, length = arr.length; i < length; i++) {
+        const value = arr[i];
+        if (Array.isArray(value)) {
+            flatten(value, result);
+        } else {
+            result.push(value);
+        }
+    }
+    return result;
+}
+
+async function getUserPostCandidates(user: UserModel): Promise<DocumentReference[]> {
+    const followedUsers = Promise.all(user.usersFollowing.map(async user => {
+        const followedUser = (await user.get()).data() as UserModel;
+        return followedUser.createdPosts;
+    }));
+    const followedGenres = Promise.all(user.genresFollowing.map(async genre => {
+        const followedGenre = await firestore.collection('genres').doc(genre).get();
+        const followedGenreData = followedGenre.data() as GenreModel;
+        return followedGenreData.posts;
+    }));
+    const unprocessedCandidates = await Promise.all([followedUsers, followedGenres]);
+    const mergedUnprocessedCandidates = flatten(unprocessedCandidates);
+    return mergedUnprocessedCandidates.filter((item, index) => {
+        return index === mergedUnprocessedCandidates.indexOf(item);
+    });
+}
+
+interface PostModel {
+    anon: boolean;
+    comments: DocumentReference[];
+    content: string;
+    created: Timestamp;
+    downvoteCount: number;
+    downvoteList: DocumentReference[];
+    genre: string;
+    poster: DocumentReference;
+    title: string;
+    upvoteCount: number;
+    upvoteList: DocumentReference[];
+}
+
+interface CommentModel {
+    poster: DocumentReference;
+    parent: DocumentReference;
+    content: string;
+}
+
+async function postRelevancy(postReference: DocumentReference, userReference: DocumentReference, user: UserModel): Promise<number> {
+    // upvotes - downvotes + 2 * comments
+    const postData = (await postReference.get()).data();
+    if (!postData) return -1;
+    const post = postData as PostModel;
+    if (post.poster == userReference) return Number.MIN_VALUE;
+    const comments = await Promise.all(post.comments.map(async reference => {
+        return (await reference.get()).data() as CommentModel;
+    }))
+
+    const isSaved = user.savedPosts.includes(postReference);
+    const posterIsFollowed = user.usersFollowing.includes(post.poster);
+    const genreIsFollowed = user.genresFollowing.includes(post.genre);
+    const hasUpvoted = post.upvoteList.includes(userReference);
+    const hasDownvoted = post.downvoteList.includes(userReference);
+    const commentsMade = comments.filter(comment => comment.poster == userReference).length;
+
+    let relevanceScore = 0;
+    relevanceScore += hasDownvoted? -1 : 0;
+    relevanceScore += hasUpvoted? 1 : 0;
+    relevanceScore += 2 * commentsMade;
+    relevanceScore += isSaved? 2 : 0;
+
+    if (!posterIsFollowed && genreIsFollowed) relevanceScore *= 1;
+    else if (posterIsFollowed && !genreIsFollowed) relevanceScore *= 2;
+    else relevanceScore *= 3;
+
+    return relevanceScore;
+}
+
+const POST_LIMIT_DEFAULT = 10;
+const POST_LIMIT_MAXIMUM = 30;
+
+async function relevantPosts(req: https.Request): Promise<RelevantPostResponse> {
+    if (!req.query.uid) return new RelevantPostResponse();
+    const uid = req.query.uid.toString();
+    let maxPosts = POST_LIMIT_DEFAULT;
+    if (req.query.limit) {
+        const limit = +req.query.limit;
+        if (limit > 0 && limit < POST_LIMIT_MAXIMUM) {
+            maxPosts = limit;
+        }
+    }
+
+    const userReference = firestore.collection('users').doc(uid);
+    const userData = (await userReference.get()).data()
+    if (!userData) return new RelevantPostResponse();
+    const user = userData as UserModel;
+
+    const postCandidates = await getUserPostCandidates(user);
+    const postScores = await Promise.all(postCandidates.map(post => postRelevancy(post, userReference, user)));
+    const candidatesWithScores = postScores.map((score, index) => {
+        return {
+            score: score,
+            post: postCandidates[index],
+        };
+    });
+    const mostRelevantCandidates = candidatesWithScores
+        .sort((a, b) => a.score - b.score)
+        .slice(0, maxPosts);
+    const result = await Promise.all(mostRelevantCandidates.map(pair => pair.post.get()));
+
+    return {
+        posts: result.map(post => post.data() as PostModel),
+    };
 }
